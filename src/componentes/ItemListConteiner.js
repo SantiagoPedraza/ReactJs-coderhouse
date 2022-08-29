611d4a771907353043b0d82d70ba01afb882e13a
santiago pedraza <103968703+SantiagoPedraza@users.noreply.github.com>
2022-08-29 19:46:20 -0300

    .
    .

--- src/componentes/ItemListConteiner.js
+++ src/componentes/ItemListConteiner.js
@@ -1,8 +1,13 @@
+import './navstyle.css';
+import Contador from './ItemCount';
+
 const ItemListConteiner = ({titulo})=>{
     return(
         <div>
-            <h2>titulo</h2>
+            <h2>NUESTRAS VARIEDADES</h2>
+            <Contador/>
         </div>
+           
 
     );
 }
