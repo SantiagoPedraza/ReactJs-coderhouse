0f4785b1ad2533eac008085bba9ad9eb3052cc77
santiago pedraza <103968703+SantiagoPedraza@users.noreply.github.com>
2022-09-28 22:31:08 -0300

    correccion del numero de orden final
    correccion del numero de orden final

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -33,41 +33,56 @@ export const CartProvider = ({children}) => {
         return cart.reduce((acc, item) => acc + item.cantidad * item.precio, 0)
     }
 
-    const emptyCart = () => {
-        Swal.fire({
-            title: 'Estas seguro?',
-            text: "Vaciaras todo el carro!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'si,lo estoy!'
-          }).then((result) => {
-            if (result.isConfirmed) {
-                setCart([])
-            }
-          })
-    }
+    const emptyCart=()=>{
+      Swal.fire({
+          title: 'Are you sure?',
+          text: "You won't be able to revert this!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+          if (result.isConfirmed) {
+              setCart([])
+            Swal.fire(
+              'Deleted!',
+              'Your file has been deleted.',
+              'success'
+            )
+          }
+        })
+      
+  }
 
-    useEffect(() => {
-        localStorage.setItem('carrito', JSON.stringify(cart))
-    }, [cart])
+  const terminarCompra = (id)=>{
+      Swal.fire({
+          title: 'Compra exitosa',
+          text: `Tu numero de orden es: ${id}`,
+          icon: 'success',
+          confirmButtonColor: '#3085d6',
+          confirmButtonText: 'OK'
+        })
+        setCart([])
+  }
+  return (
+      <CartContext.Provider value={{
+          cart,
+          addToCart,
+          isInCart,
+          cartQuantity,
+          cartTotal,
+          emptyCart,
+          removeItem,
+          terminarCompra,
+      }
+      }>
+          {children}
 
-    return (
-        <CartContext.Provider value={{
-            cart,
-            addToCart,
-            isInCart,
-            cartQuantity,
-            cartTotal,
-            emptyCart,
-            removeItem
-          }}>
-            {children}
-        </CartContext.Provider>
-    )
+      </CartContext.Provider>
+  )
 }
 
 export const useCartContext = () => {
-    return useContext(CartContext)
+  return useContext(CartContext)
 }
